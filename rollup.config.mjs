81ae6ac4055fa8eef456fd07cfed68b29cc75af3
MBJ <thiswallz@gmail.com>
2022-10-29 13:19:32 -0300

    remove unused import

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
-import { visualizer } from "rollup-plugin-visualizer";
 
 import pkg from './package.json' assert { type: 'json' };
 
